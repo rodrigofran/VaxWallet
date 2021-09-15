9e55a7d3076a26a28188c13d6d09bbad0bad6cea
rodrigofran <rodrigofrancms@gmail.com>
2021-09-15 11:50:23 -0300

    ajuste tela login e perfil

--- screens/TabProfileScreen.tsx
+++ screens/TabProfileScreen.tsx
@@ -9,6 +9,8 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { Feather } from '@expo/vector-icons';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
 const FirstName = 'Rodrigo'
 const LastName = 'Occhiuto'
@@ -16,6 +18,9 @@ const LastName = 'Occhiuto'
 
 
 export default function TabProfileScreen() {
+
+  const navigation = useNavigation()
+
   return (
     
     <SafeAreaView style={styles.container}>
@@ -25,7 +30,7 @@ export default function TabProfileScreen() {
             source={{
               uri: 'https://i.pinimg.com/originals/4c/41/ef/4c41eff22888e5e5d8277cf5121691db.png'
             }}
-            size={140}
+            size={120}
           />
           <View style={styles.ViewTitle}>
             <Text style={styles.title}>Olá, {FirstName}</Text>
@@ -33,28 +38,27 @@ export default function TabProfileScreen() {
         </View>
         <View style={styles.detail}>
           <View style={styles.input}>
-            <FontAwesome name='user-o'size={20} style={styles.icon}/>
-            <Text style={styles.content}>{FirstName}</Text>
+            <FontAwesome name='user-o'size={17} style={styles.icon}/>
+            <Text style={styles.content}>{FirstName} {LastName}</Text>
           </View>
           <View style={styles.input}>
-            <FontAwesome name='user-o'size={20} style={styles.icon}/>
-            <Text style={styles.content}>{LastName}</Text>
-          </View>
-          <View style={styles.input}>
-            <Feather name='phone'size={20} style={styles.icon}/>
+            <Feather name='phone'size={17} style={styles.icon}/>
             <Text style={styles.content}>Celular</Text>
           </View>
           <View style={styles.input}>
-            <FontAwesome name='envelope-o'size={20} style={styles.icon}/>
+            <FontAwesome name='envelope-o'size={17} style={styles.icon}/>
             <Text style={styles.content}>Email</Text>
           </View>
           <View style={styles.input}>
-            <FontAwesome name='globe'size={20} style={styles.icon}/>
+            <FontAwesome name='globe'size={17} style={styles.icon}/>
             <Text style={styles.content}>País</Text>
           </View>
           <View style={styles.input}>
-            <Icon name='map-marker-outline'size={20} style={styles.icon}/>
+            <Icon name='map-marker-outline'size={17} style={styles.icon}/>
             <Text style={styles.content}>Cidade</Text>
+          </View>
+          <View  style = {styles.logout}>
+          <Button mode='contained'onPress={() => navigation.navigate('LoginScreen')} >Sair</Button>
           </View>
           
         </View>
@@ -88,11 +92,11 @@ const styles = StyleSheet.create({
   },
 
   title: {
-    fontSize: 20
+    fontSize: 17
   },
 
   content: {
-    fontSize: 20,
+    fontSize: 17,
     color: 'grey',
     
   },
@@ -102,7 +106,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     height: 500,
     width: '100%',
-    marginTop: 20
+    marginTop: 20,
   },
 
   input: {
@@ -120,6 +124,11 @@ const styles = StyleSheet.create({
   icon: {
     marginRight: 10
   },
+
+  logout: {
+    width: '30%',
+    marginBottom: 20
+  }
 
   
 
